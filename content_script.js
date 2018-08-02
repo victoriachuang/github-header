edaf6b56eea417b4c7416aa6c02a4df438d7c20d
Danny Andrews <lunde.andrews@gmail.com>
2018-08-02 14:59:44 -0500

    Use es6 features since they are fully supported in Chrome

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-var colors = [
+const COLORS = [
   // reds
   "#F88379", // tea rose
   "#EF597B", // rose
@@ -33,39 +33,39 @@ var colors = [
   "#9D549C", // beet
 ];
 
-var ls = localStorage;
-var lsNamespace = 'CE.GITHUB_HEADER';
-var opts = {
-  partyMode: localStorage[lsNamespace + '.PARTY_MODE'] === 'true',
-  colorChangeDelay: Number(localStorage[lsNamespace + '.COLOR_CHANGE_DELAY']) || 200,
-  animationSpeed: localStorage[lsNamespace + '.HEADER_SWIRL_DURATION'] || '1s'
+const getOption = option => localStorage[`${lsNamespace}.${option}`];
+const lsNamespace = 'CE.GITHUB_HEADER';
+const opts = {
+  partyMode: getOption('PARTY_MODE') === 'true',
+  colorChangeDelay: Number(getOption('COLOR_CHANGE_DELAY')) || 200,
+  animationSpeed: getOption('HEADER_SWIRL_DURATION') || '1s'
 };
 
-function pickColor() {
-  var randomColor = colors[Math.floor((Math.random() * colors.length))];
+const pickColor = () => {
+  const randomColor = COLORS[Math.floor((Math.random() * COLORS.length))];
   document.getElementsByClassName('Header')[0].style.backgroundColor = randomColor;
   document.getElementsByClassName('header-search-scope')[0].style.borderRightColor = randomColor;
   document.getElementsByClassName('mail-status')[0].style.borderColor = randomColor;
-}
+};
 
-function alternateColors() {
-  setTimeout(function() {
+const alternateColors = () => {
+  setTimeout(() => {
     pickColor();
     alternateColors();
   }, opts.colorChangeDelay);
 };
 
-function rotateHeader() {
-  var header = document.querySelector('.Header');
+const rotateHeader = () => {
+  const header = document.querySelector('.Header');
   header.style['z-index'] = 1000;
   header.style.position = 'relative';
   header.style.animation = 'circle ' + opts.animationSpeed + ' infinite linear';
-}
+};
 
-function startTheParty() {
+const startTheParty = () => {
   alternateColors();
   rotateHeader();
-}
+};
 
 if(opts.partyMode) {
   startTheParty()
